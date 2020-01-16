f365881cb3f32458cfaa09c73d8ba087787ec743
Jesper Pedersen <jesper@yepz.dk>
2020-01-16 15:11:46 +0100

    4671: Add btn class

--- templates/ddb-cover-upload-meta-left.tpl.php
+++ templates/ddb-cover-upload-meta-left.tpl.php
@@ -1,7 +1,6 @@
 <div class="overflow-hidden">
-  <input type="button" class="form-submit" value="Edit image" />
+  <p><a href="#" class="btn btn-primary"><?php print t('Adapt image'); ?></a></p>
   <!-- @TODO: Placehold image only for class demo purpose. -->
   <img src="//placehold.it/1000x1000" alt="" class="img-responsive">
-  <p><a href="#"><?php print t('Adapt image'); ?></a></p>
   <?php print $variables['image']; ?>
 </div>
