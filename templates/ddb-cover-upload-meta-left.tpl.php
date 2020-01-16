e183ba66690d41d4e95b6b64b92af103b347ab4d
Jesper Pedersen <jesper@yepz.dk>
2020-01-16 15:06:58 +0100

    4671: Add icons and markup success

--- templates/ddb-cover-upload-meta-left.tpl.php
+++ templates/ddb-cover-upload-meta-left.tpl.php
@@ -1,3 +1,6 @@
-<div>
+<div class="overflow-hidden">
+  <input type="button" class="form-submit" value="Edit image" />
+  <!-- @TODO: Placehold image only for class demo purpose. -->
+  <img src="//placehold.it/1000x1000" alt="" class="img-responsive">
   <?php print $variables['image']; ?>
 </div>
