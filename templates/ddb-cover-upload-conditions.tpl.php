3ed34706178cd5b0be305637aca8e7301e3ca27a
Martin Yde Granath <my@aarhus.dk>
2020-01-16 09:29:52 +0100

    first commit

---
+++ templates/ddb-cover-upload-conditions.tpl.php
@@ -0,0 +1,8 @@
+<div>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id viverra urna. Sed hendrerit suscipit ultricies. Ut vestibulum nulla vel elementum semper. Proin tristique urna pretium neque tempor, ac auctor tellus posuere. Curabitur facilisis odio vel cursus pellentesque. Donec vitae elementum purus, sed pretium sapien. Aliquam erat volutpat. Vestibulum consequat velit et libero iaculis, eu lobortis lectus faucibus. Vestibulum quis neque vitae justo ornare semper at vitae tellus. Nam elementum pretium congue. Sed blandit ornare nulla, at imperdiet ante semper et. Sed sit amet urna nec sem condimentum placerat a id sem. Donec vestibulum felis ligula, vitae sagittis arcu vehicula non. Sed imperdiet elit vitae nunc pulvinar ullamcorper. Cras placerat quam ligula, a auctor purus ullamcorper eget. Pellentesque dictum, ipsum ac euismod volutpat, lorem augue porta nulla, ut dignissim nisl nulla ac mauris.
+  </p>
+  <p>
+    Proin venenatis justo est, eu bibendum lorem fermentum a. Donec ac diam volutpat, fermentum dolor vitae, feugiat urna. Sed hendrerit, mauris quis congue semper, augue est elementum dui, eget rutrum erat mauris nec quam. Suspendisse molestie sollicitudin risus quis efficitur. Ut auctor feugiat semper. Donec scelerisque velit id egestas tristique. Fusce gravida, lectus ut porttitor tempor, dui nibh molestie ipsum, vitae molestie neque lectus ut enim. Integer enim risus, interdum sit amet neque ac, tristique hendrerit purus. Quisque elementum vel justo eu dapibus. Morbi maximus lectus dignissim, tristique sem vitae, suscipit dolor. In aliquam justo vitae tellus lacinia interdum. Donec eu ante accumsan, efficitur est in, ullamcorper eros.
+  </p>
+</div>
