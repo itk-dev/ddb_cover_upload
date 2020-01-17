8400817087e3d1553a3155f1a0062307ff060dc6
Martin Yde Granath <my@aarhus.dk>
2020-01-17 09:31:13 +0100

    4671: Updated metadata page

--- templates/ddb-cover-upload-meta-left.tpl.php
+++ templates/ddb-cover-upload-meta-left.tpl.php
@@ -1,6 +1,5 @@
 <div class="overflow-hidden">
   <p><a href="#" class="btn btn-primary"><?php print t('Adapt image'); ?></a></p>
   <!-- @TODO: Placehold image only for class demo purpose. -->
-  <img src="//placehold.it/1000x1000" alt="" class="img-responsive">
   <?php print $variables['image']; ?>
 </div>
